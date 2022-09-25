a120bce8cd7dc2461725f3f8c8ff818d5442fd99
Kathrine <kath415a@stud.kea.dk>
2022-09-25 22:37:20 +0200

    js still doesnt work

--- js/product.js
+++ js/product.js
@@ -11,12 +11,29 @@ const options = {
 //632c555dbf647d0a5c19864e
 
 fetch(url, options)
-  .then((respons) => {
-    if (!respons.ok) {
+  .then((response) => {
+    if (!response.ok) {
       throw Error(response.statusText);
     }
-    return respons.json();
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
   })
   .catch((e) => {
     console.error("an error occured:", e.message);
   });
+
+function handleData(stardustlab) {
+  stardustlab.forEach((product) => {
+    console.log(product);
+
+    const template = document.querySelector("#producttemp").content;
+
+    const clone = template.cloneNode(true);
+    clone.querySelector(".price").textContent = product.price;
+
+    const parent = document.querySelector("main");
+    parent.appendChild(clone);
+  });
+}
---
+++ js/productlist.js
@@ -0,0 +1,40 @@
+const url = "https://stardustlab-023b.restdb.io/rest/stardustlab?max=22";
+
+const options = {
+  headers: {
+    "x-apikey": "632c555dbf647d0a5c19864e",
+  },
+};
+
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.error("an error occured:", e.message);
+  });
+
+//   .then((res) => res.json())
+//   .then((data) => handleData(data));
+
+function handleData(stardustlab) {
+  stardustlab.forEach((product) => {
+    //console.log(product);
+
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+
+    clone.querySelector(
+      ".item"
+    ).textContent = `${product.articletype} | ${product.productdisplayname}`;
+
+    const parent = document.querySelector("main");
+    parent.appendChild(clone);
+  });
+}
